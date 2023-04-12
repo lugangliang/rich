868a0951340adfca0f7c6e29219a43be28049552
lugangliang <lugangliang555@gamil.com>
2023-04-12 11:41:42 +0800

    两种方式实现了单链表的反转。

--- cmd/data/list/signallist.go
+++ cmd/data/list/signallist.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type SingleList struct {
@@ -20,8 +21,8 @@ func newNode(data int) *SingleList {
 
 func (list *SingleList) insertNodeToHead(node *SingleList) {
 	if list.next == nil {
-		fmt.Printf("%p\n", &list)
 		list.next = node
+		node.next = nil
 		return
 	}
 
@@ -32,6 +33,7 @@ func (list *SingleList) insertNodeToHead(node *SingleList) {
 func (list *SingleList) insertNodeToEnd(node *SingleList) {
 	if list.next == nil {
 		list.next = node
+		node.next = nil
 		return
 	}
 
@@ -46,31 +48,45 @@ func (list *SingleList) insertNodeToEnd(node *SingleList) {
 
 }
 
-func (list *SingleList) reserveSingleList() {
+func (list *SingleList) reserveSingleListMethod1() {
 
 	newHead := newNode(0)
 
-	if list.next == nil {
+	// empty list or only one node.
+	if list.next == nil || list.next.next == nil {
 		return
 	}
 
-	if list.next.next == nil {
-		return
-	}
-
-	tmp := list.next
-	for tmp != nil {
+	for tmp := list.next; tmp != nil; {
+		// record tmp's next pointer
+		next := tmp.next
 		newHead.insertNodeToHead(tmp)
-		tmp = tmp.next
+		tmp = next
 	}
 
 	list.next = newHead.next
+}
 
+func (list *SingleList) reserveSingleListMethod2() {
+	// empty list or only one node.
+	if list.next == nil || list.next.next == nil {
+		return
+	}
+
+	secondElem := list.next
+
+	// 依次将下一个元素的下一个元素插到头上去。
+	for tmp := list.next.next; tmp != nil; {
+		next := tmp.next
+		secondElem.next = next
+		list.insertNodeToHead(tmp)
+		tmp = next
+	}
 }
 
 func newList() *SingleList {
 	head := newNode(0)
-	for i := 0; i < 20; i++ {
+	for i := 2; i < 30; i++ {
 		tmp := newNode(i)
 		(head).insertNodeToHead(tmp)
 	}
@@ -82,7 +98,8 @@ func (list *SingleList) PrintList() {
 		fmt.Println("empty list.")
 	}
 
-	tmp := list
+	// ignore first element.
+	tmp := list.next
 	for tmp != nil {
 		fmt.Printf(" %d,", tmp.data)
 		tmp = tmp.next
@@ -93,7 +110,13 @@ func (list *SingleList) PrintList() {
 func main() {
 	head := newList()
 	head.PrintList()
-	head.reserveSingleList()
+
+	time.Sleep(2 * time.Second)
+	head.reserveSingleListMethod1()
+	head.PrintList()
+
+	time.Sleep(2 * time.Second)
+	head.reserveSingleListMethod2()
 	head.PrintList()
 	fmt.Println("Learn single list.")
 }
